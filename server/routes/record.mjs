835eaf201e046eab5b34c2eeb5268c57c0c5306e
AllComein <144424426+AllComein@users.noreply.github.com>
2023-10-06 10:24:07 +0530

    Create record.mjs

---
+++ server/routes/record.mjs
@@ -0,0 +1,85 @@
+import express from "express";
+import db from "../db/conn.mjs";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+// This section will help you get a list of all the records.
+router.get("/", async (req, res) => {
+  let collection = await db.collection("records");
+  let results = await collection.find({}).toArray();
+  res.send(results).status(200);
+});
+
+// This section will help you get a single record by id
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("records");
+  let query = {_id: new ObjectId(req.params.id)};
+  let result = await collection.findOne(query);
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+// This section will help you create a new record.
+router.post("/", async (req, res) => {
+  let newDocument = {
+    name: req.body.name,
+    position: req.body.position,
+    level: req.body.level,
+  };
+  let collection = await db.collection("records");
+  let result = await collection.insertOne(newDocument);
+  res.send(result).status(204);
+});
+
+// This section will help you update a record by id.
+router.patch("/:id", async (req, res) => {
+  const query = { _id: new ObjectId(req.params.id) };
+  const updates =  {
+    $set: {
+      name: req.body.name,
+      position: req.body.position,
+      level: req.body.level
+    }
+  };
+
+  let collection = await db.collection("records");
+  let result = await collection.updateOne(query, updates);
+
+  res.send(result).status(200);
+});
+
+// This section will help you delete a record
+router.delete("/:id", async (req, res) => {
+  const query = { _id: new ObjectId(req.params.id) };
+
+  const collection = db.collection("records");
+  let result = await collection.deleteOne(query);
+
+  res.send(result).status(200);
+});
+
+
+router.get("/file/:id", async (req, res) => {
+  const query = { _id: new ObjectId(req.params.id) };
+
+  const collection = db.collection("records");
+  const result = await collection.findOne(query);
+
+  if (!result || !result.fileUrl) {
+    res.status(404).send("File not found");
+  } else {
+    // Set the Content-Disposition header to force download or inline display
+    res.setHeader(
+      "Content-Disposition",
+      `inline; filename="${result.fileName}"`
+    );
+    // You may need to set the appropriate content type based on the file type
+    res.setHeader("Content-Type", "application/octet-stream");
+    // Send the file
+    res.sendFile(result.fileUrl);
+  }
+});
+
+export default router;
